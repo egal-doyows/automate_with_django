579379ce0e4ecefaa5eb4847e82670a25406bcbb
egal-doyows <egaldoyows@gmail.com>
2025-03-03 20:18:44 +0300

    created two custom commands helloworld and greeting

---
+++ dataentry/management/commands/greeting.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+#proposed output
+
+
+class Command(BaseCommand):
+    help = "Greets the users"
+    def add_arguments(self, parser):
+        parser.add_argument('name', type=str,help="specifies username")
+        
+
+    def handle(self , *args, **kwargs):
+        name=kwargs['name']
+        greeting=f'Hi, {name}, Good Evening'
+        self.stdout.write(self.style.SUCCESS(greeting))
---
+++ dataentry/management/commands/helloworld.py
@@ -0,0 +1,5 @@
+from django.core.management.base import BaseCommand
+class Command(BaseCommand):
+    help=" Prints Hello world"
+    def handle(self, *args, **options):
+        self.stdout.write("hello world")
